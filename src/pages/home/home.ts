486daa58d2931d653abe1e512218c72851797dd4
Juanf <juanfa107@gmail.com>
2020-04-21 23:29:34 -0500

    commit inicial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,46 @@
+import {Component} from "@angular/core";
+import {NavController, PopoverController} from "ionic-angular";
+import {Storage} from '@ionic/storage';
+
+import {NotificationsPage} from "../notifications/notifications";
+import {SettingsPage} from "../settings/settings";
+import {TripsPage} from "../trips/trips";
+import {SearchLocationPage} from "../search-location/search-location";
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+  // search condition
+  public search = {
+    name: "Rio de Janeiro, Brazil",
+    date: new Date().toISOString()
+  }
+
+  constructor(private storage: Storage, public nav: NavController, public popoverCtrl: PopoverController) {
+  }
+
+  ionViewWillEnter() {
+    
+  }
+
+
+  // to go account page
+  goToAccount() {
+    this.nav.push(SettingsPage);
+  }
+
+  presentNotifications(myEvent) {
+    console.log(myEvent);
+    let popover = this.popoverCtrl.create(NotificationsPage);
+    popover.present({
+      ev: myEvent
+    });
+  }
+
+}
+
+//
